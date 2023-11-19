f3b57afd7c0b49ba92b84f30409c07ab6e1ec4fb
Master Mind <48193931+iboss21@users.noreply.github.com>
2023-11-18 18:45:21 -0600

    Create fxmanifest.lua
    rewrite

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'iBoss21'
+description 'Dumplocker resource for QBCore'
+version '1.0.0'
+
+client_script 'client/client.lua'
+server_script 'server/server.lua'
+shared_script 'config.lua'
